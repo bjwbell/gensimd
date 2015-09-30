4ffe929fd91fabba7a1d1198212acf9030a57bf4
Bryan Bell <bryan.w.bell@gmail.com>
2015-09-30 10:12:44 -0700

    Flip simd build tag, fix build err, and remove example binary

--- example/simd_loop1.go
+++ example/simd_loop1.go
@@ -1,4 +1,4 @@
-// +build simd_go
+// +build !simd
 
 //go:generate gensimd
 
@@ -14,14 +14,14 @@ func simd_loop1(v4 *[4]int) error {
 	v := simd.Int4Var(v4)
 	loop := simd.ForLoop{
 		Start: 0, Iterations: 10, StepBy: 0,
-		Body: []simd.Instruction{
+		Body: []simd.Instructioner{
 			simd.Int4Add{Result: v, A: v, B: v},
 		},
 	}
 	f := simd.Func{
-		Init:   []simd.Instruction{},
+		Init:   []simd.Instructioner{},
 		Loop:   loop,
-		Finish: []simd.Instruction{},
+		Finish: []simd.Instructioner{},
 		Ret:    &simd.RetSuccess{}}
 	fmt.Println("loop:", loop)
 	return f.Exec()
